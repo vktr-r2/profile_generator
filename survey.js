d71f1e1a6ad662a2b2e753891be86941580bb541
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-12 01:03:22 -0400

    Add pseudo and require/setup realine

---
+++ survey.js
@@ -0,0 +1,16 @@
+//PSEUDO
+
+//Sounds like we need to ask question, receive user input and store, later add input to unique paragraph.
+
+//HOW: Ask question by console.logging
+//HOW: Receive and store user input by using process.argv
+
+///////////////////////////////////////////////////////////////////////////////////
+
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
